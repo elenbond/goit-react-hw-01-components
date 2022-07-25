e55127a303c9992751d54ea7eccf960c081460ef
Olena Bondiuchna <93439766+elenbond@users.noreply.github.com>
2022-07-25 16:26:22 +0300

    fixed styles in files {name}.module.css

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,9 +1,9 @@
 import PropTypes from 'prop-types';
 import css from './Statistics.module.css';
 
-export default function Statistics({ title, stats }) { 
+export default function Statistics({ title = 'upload stats', stats }) { 
     return (
-        <section>
+        <section className={css.statistics}>
             {title && <h2 className={css.title}>{title}</h2>}
             <ul className={css.list}>
                 {stats.map(item => (
